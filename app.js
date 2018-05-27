3abef6e90b9e93bccbebd1a91859bd838fde595c
ilandev <ilan.auctionsoftware@gmail.com>
2018-05-27 21:59:10 +0530

    done

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 var express  = require('express')
 var app = express();
 var bodyParser = require('body-parser')
+var firebase = require('firebase')
 
 var cors = require('cors');
+
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDKKFfMvZYj_jvVFL3hBZFRckZBZqpuuJM",
+    authDomain: "trackbuddy-704b3.firebaseapp.com",
+    databaseURL: "https://trackbuddy-704b3.firebaseio.com",
+    projectId: "trackbuddy-704b3",
+    storageBucket: "trackbuddy-704b3.appspot.com",
+    messagingSenderId: "195476621352"
+  };
+  firebase.initializeApp(config);
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.post('/login',(req,res)=>{
+  console.log(req.body)
+  res.send(req.body)
+})
+app.post('/register',(req,res)=>{
   console.log(req.body)
   res.send(req.body)
 })
